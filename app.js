67be1d007f1b0425aab16de2618b30627d069146
LikhithGowdaBR <likhithgowda08052005@gmail.com>
2026-07-26 20:57:22 +0530

    Implement server-side review creation and validation

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
-const { listingSchema } = require("./schema.js");
+const { listingSchema, reviewSchema } = require("./schema.js");
 const Review = require("./models/review.js");
 
 main()
@@ -34,6 +34,16 @@ app.use(express.static(path.join(__dirname, "/public")));
 
 const validateListing = (req, res, next) => {
   let { error } = listingSchema.validate(req.body);
+  if (error) {
+    let errMsg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(400, errMsg);
+  } else {
+    next();
+  }
+};
+
+const validateReview = (req, res, next) => {
+  let { error } = reviewSchema.validate(req.body);
   if (error) {
     let errMsg = error.details.map((el) => el.message).join(",");
     throw new ExpressError(400, errMsg);
@@ -108,17 +118,21 @@ app.get("/listings/:id", async (req, res) => {
 
 //reviews
 //Post Route
-app.post("/listings/:id/reviews", async (req, res) => {
-  let listing = await Listing.findById(req.params.id);
-  let newReview = new Review(req.body.review);
+app.post(
+  "/listings/:id/reviews",
+  validateReview,
+  wrapAsync(async (req, res) => {
+    let listing = await Listing.findById(req.params.id);
+    let newReview = new Review(req.body.review);
 
-  listing.reviews.push(newReview);
+    listing.reviews.push(newReview);
 
-  await newReview.save(); //to push new review
-  await listing.save(); //to change in existing doc
+    await newReview.save(); //to push new review
+    await listing.save(); //to change in existing doc
 
-  res.redirect(`/listings/${listing._id}`);
-});
+    res.redirect(`/listings/${listing._id}`);
+  }),
+);
 
 app.use((req, res, next) => {
   next(new ExpressError(404, "Page Not Found!"));
