81b5f1d26e8aca13eba71cbfe1638b029f9342ea
LikhithGowdaBR <likhithgowda08052005@gmail.com>
2026-07-29 17:44:28 +0530

    Refactor listings and reviews using MVC architecture

--- routes/review.js
+++ routes/review.js
@@ -10,24 +10,15 @@ const {
   isReviewAuthor,
 } = require("../middleware.js");
 
+const reviewController = require("../controllers/review.js");
+
 //reviews
 //Post Route
 router.post(
   "/", // listings/:id/reviews-> /
   isLoggedIn,
   validateReview,
-  wrapAsync(async (req, res) => {
-    let listing = await Listing.findById(req.params.id);
-    let newReview = new Review(req.body.review);
-    newReview.author = req.user._id; //review author name
-    listing.reviews.push(newReview);
-
-    await newReview.save(); //to push new review
-    await listing.save(); //to change in existing doc
-    req.flash("success", "New Review Created!");
-
-    res.redirect(`/listings/${listing._id}`);
-  }),
+  wrapAsync(reviewController.createReview),
 );
 
 //Review Delete Route
@@ -35,15 +26,7 @@ router.delete(
   "/:reviewId",
   isLoggedIn,
   isReviewAuthor,
-  wrapAsync(async (req, res) => {
-    let { id, reviewId } = req.params;
-
-    await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
-    await Review.findByIdAndDelete(reviewId);
-    req.flash("success", "Review Deleted!");
-
-    res.redirect(`/listings/${id}`);
-  }),
+  wrapAsync(reviewController.destroyReview),
 );
 
 module.exports = router;
