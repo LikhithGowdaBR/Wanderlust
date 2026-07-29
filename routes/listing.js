31cd3e1eedda2f1fc608de9d8abfbac0dda3181b
LikhithGowdaBR <likhithgowda08052005@gmail.com>
2026-07-29 23:23:32 +0530

    Integrate Cloudinary image upload setup

--- routes/listing.js
+++ routes/listing.js
@@ -4,6 +4,9 @@ const wrapAsync = require("../utils/wrapAsync.js");
 const Listing = require("../models/listing.js");
 const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
 const listingController = require("../controllers/listings.js");
+const multer = require("multer");
+const { storage } = require("../cloudConfig.js");
+const upload = multer({ storage });
 
 //Index Route and Create Route
 router
@@ -12,6 +15,7 @@ router
   .post(
     isLoggedIn,
     validateListing,
+    upload.single("listing[image]"),
     wrapAsync(listingController.createListing),
   );
 
